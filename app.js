e0eb22de4a9445fb44b48c281924fb40df2844e6
Alao-Abiodun <46402042+Alao-Abiodun@users.noreply.github.com>
2022-05-30 13:30:17 +0100

    chore:boostraping the app setup

---
+++ app.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+// const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+const key = require("./utils/libs/gen-keys");
+
+const globalErrorHandler = require("./controllers/error.controller");
+
+dotenv.config();
+
+if (process.env.NODE_ENV === "production") {
+  process.env.FIXERS_ACCESS_TOKEN_SECRET = key(64);
+  process.env.FIXERS_COOKIE_SECRET = key(64);
+}
+
+const app = express();
+
+// Set Security HTTP Headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Limit Request from same API
+// const limiter = rateLimit({
+//   max: 1000,
+//   windowMs: 60 * 60 * 1000,
+//   message: "Too many request from this IP, please try again in an hour!",
+// });
+
+//app.use("/api", limiter);
+
+app.use(cors());
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("views"));
+
+// Data sanitize against NoSQL Query Injection
+app.use(mongoSanitize()); // Checks the request headers, query strings, params for malicious codes
+
+// Import all routes
+
+//default Route
+app.get("/", (req, res) => {
+  res.json({ message: `Welcome to Fixers API v1` });
+});
+
+// Home Route
+app.get("/api/v1/home", (req, res) => {
+  res.json({ message: `Welcome to Fixers API v1` });
+});
+
+app.post("/api/v1/withdrawal", (req, res) => {
+  res.status(200).send("OK");
+});
+
+//   Routes Middleware
+// app.use("/api/v1/auth", authRouter);
+// app.use("/api/v1/user", userRouter);
+
+// Unhandled Routes
+app.all("*", (req, res) => {
+  res
+    .status(404)
+    .json({ message: `Can't find resource ${req.originalUrl} on this server` });
+});
+
+// Global Error Handler
+app.use(globalErrorHandler);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,81 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const chalk = require("chalk");
+
+// UNCAUGHT EXCEPTIONS
+// Application needs to be crashed then a tool will be needed to restart the APP
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! ...");
+  console.log({ err });
+  console.log(err.name, err.message);
+  process.exit;
+});
+
+const app = require("./app");
+
+const DB = process.env.FIXERS_DB.replace(
+  "<password>",
+  process.env.FIXERS_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(chalk.green("Connected to DB successfully"));
+  });
+
+mongoose.connection.on("error", (err) => console.log(err.message));
+mongoose.connection.on("disconnected", () => {
+  console.log(chalk.red("Mongoose connection closed"));
+});
+
+// START SERVER
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+  if (!Number.isNaN(port)) {
+    return val;
+  }
+
+  if (port > 0) {
+    return port;
+  }
+
+  return false;
+};
+
+const port = normalizePort(process.env.PORT || "8888");
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+// create a http server
+const server = app.listen(port, async () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? `pipe ${address}` : `port ${port}`;
+  console.log(`Listening on ${chalk.green(bind)}`);
+});
+
+// Catching Exceptions
+
+// Application does not necessarily need to be crashed
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION!...");
+  console.log(err.name, err.message);
+  console.log({ err });
+  server.close(() => {
+    process.exit();
+  });
+});
+
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  process.exit(0);
+});
